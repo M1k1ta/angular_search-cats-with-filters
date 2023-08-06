72952dcc1d13b2fedbd29b5d79136581c90efb57
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-08-06 13:42:04 +0300

    filter form and list ready

--- src/app/utils/client.ts
+++ src/app/utils/client.ts
@@ -3,6 +3,4 @@ import axios from 'axios';
 export const client = axios.create({
   baseURL: 'https://api.thecatapi.com/v1',
 });
-//https://api.thecatapi.com/v1/breeds?limit=1
-//&api_key=live_t5w81Zu0VWKNY6YJSy6lSo7wO5Pqfl35DIKNDKInAI4tPXNDXLaI8gSrIeYuZ5qv
 
